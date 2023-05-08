0fc3b94e71faa541a1d1fcf30ee1bb08894e9417
Denventa Afriliyan Ferly Khurayra Shishigami X <122451961+Dvanmeploph@users.noreply.github.com>
2023-05-08 21:50:24 +0700

    Update run.py

--- run.py
+++ run.py
@@ -954,6 +954,4 @@ if __name__=='__main__':
 	if col < MC:
 		print("Ukuran layar Termux terlalu kecil. Silakan perbesar layar untuk menjalankan script ini")
 		sys.exit()
-	else:
-	    os.system('mkdir data')
-	else:Login()
+		else:Login()
